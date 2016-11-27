953dcc04415fda0969c298684e3cff8bcffcd831
xiaohigh <xiaohigh@xiaohigh.lan>
2016-11-27 20:40:27 +0800

    modify qiniu log path

--- app/Http/Controllers/QiniuController.php
+++ app/Http/Controllers/QiniuController.php
@@ -14,7 +14,7 @@ class QiniuController extends Controller
     {
         //获取请求内容
         $_body = file_get_contents('php://input');
-        file_put_contents(storage_path('log').'/qiniu.log', $_body."\r\n\r\n", FILE_APPEND);
+        file_put_contents(storage_path('logs').'/qiniu.log', $_body."\r\n\r\n", FILE_APPEND);
         //进行json解析
 //        $data = json_decode($_body, true);
 //        //查看数据
